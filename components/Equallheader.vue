7086b4e979e726a9b0ceda5860b75276232a8479
amaguri1505 <kokemomo.sour@gmail.com>
2020-02-19 15:46:20 +0900

    タイトルつける

---
+++ components/Equallheader.vue
@@ -0,0 +1,18 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" href="#">迷い犬/猫　ポスタージェネレータ</a>
+    <button
+      class="navbar-toggler"
+      type="button"
+      data-toggle="collapse"
+      data-target="#navbarSupportedContent"
+      aria-controls="navbarSupportedContent"
+      aria-expanded="false"
+      aria-label="Toggle navigation"
+    >
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+  </nav>
+</template>
+<style></style>
